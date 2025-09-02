aebf498f1c3b09abd892f604fc1b8949416dbbea
DeividPatino <DeividPatino@users.noreply.github.com>
2025-09-01 23:23:55 -0500

    Mobile

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '@angular/user';
+
 
 @Component({
   selector: 'app-login',
@@ -7,8 +11,18 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class LoginPage implements OnInit {
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]]
+  });
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private loader: LoaderProvider,
+    private toast: ToastProvider,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
